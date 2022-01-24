53b375664db562da1d99297c3ad4f69e85013669
JRSCASA <jronaldsantos@hotmail.com>
2022-01-23 23:11:16 -0300

    outras mudanças no https

--- src/public/script/home.js
+++ src/public/script/home.js
@@ -1,5 +1,4 @@
-
-function fazGet(url){
+function fazGet(url) {
     let req = new XMLHttpRequest()
     req.open("GET", url, false)
     req.send()
@@ -12,46 +11,47 @@ const center = document.querySelectorAll('.center')
 const jatem = document.getElementById('jatem')
 const destaque = document.createElement('div')
 
-function criarDiv(dados, max){
+function criarDiv(dados, max) {
     cont++
-    if(cont >= max) cont = 0
+    if (cont >= max) cont = 0
 
     let post = dados[cont]
     jatem.classList.add('jafoi')
-destaque.innerHTML = '<div class="img-destaque">'+
-                        '<div class="title-autor">'+
-                            '<img src="../public/image/fundo.jpg" />'+                            
-                            '<h4>Por <span>'+post.autor+'</span></h4>'+
-                        '</div>'+
-                        '<div class="favorito">'+
-                            '<i class="fa fa-heart" aria-hidden="true">'+post.views+'</i>'+
-                        '</div>'+
-                        '<div class="overlay-destaque"></div>'+
-                        '<div class="title-destaque-descricao">'+
-                        '<a style="text-decoration: none; color: white;" href="'+post.slug+'"> <h2>'+post.titulo+'</h2></a>'+
-                            '<p>'+post.conteudo.substr(0, 100)+'...</p>'+
-                        '</div>'+
-                        '<img id="img" src="'+post.imagen+'"/>'+
-                    '</div>';
+    destaque.innerHTML = '<div class="img-destaque">' +
+        '<div class="title-autor">' +
+        '<img src="../public/image/fundo.jpg" />' +
+        '<h4>Por <span>' + post.autor + '</span></h4>' +
+        '</div>' +
+        '<div class="favorito">' +
+        '<i class="fa fa-heart" aria-hidden="true">' + post.views + '</i>' +
+        '</div>' +
+        '<div class="overlay-destaque"></div>' +
+        '<div class="title-destaque-descricao">' +
+        '<a style="text-decoration: none; color: white;" href="' + post.slug + '"> <h2>' + post.titulo + '</h2></a>' +
+        '<p>' + post.conteudo.substr(0, 100) + '...</p>' +
+        '</div>' +
+        '<img id="img" src="' + post.imagen + '"/>' +
+        '</div>';
 
 
-center[1].appendChild(destaque)
+    center[1].appendChild(destaque)
 
 }
 
-async function main(){
-   const dados = await fazGet("http://localhost:5000/api")
+async function main() {
+    const dados = await fazGet("https://bcnews.herokuapp.com/api")
     const max = dados.length
 
-let tempo = 10000
-    function loop(){
-    
+    let tempo = 10000
+
+    function loop() {
+
         setInterval(() => {
             criarDiv(dados, max)
         }, tempo);
     }
 
-loop()
+    loop()
 }
 
 window.addEventListener("load", main)
